1e674448fb0c2785136b009997a90615346eb21b
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2016-12-02 18:59:50 +0000

    added stock and inventory

--- Specs/store_spec.js
+++ Specs/store_spec.js
@@ -15,25 +15,63 @@ describe('store', function() {
 });
 
 describe('store stock', function() {
-    it('should start off with no stock ', function() {
+    it('should start off with no stock', function() {
       var store = new Store({name:'Rocking Ricks',city:"Inverness"});
       assert.equal(0, store.box.length);
+      assert.equal(0, store.stock);
     });
     
-    it('should be able to add a record', function() {
+    it('should be able to add a record (and stock increases)', function() {
       var store = new Store({name:'Rocking Ricks',city:"Inverness"});
       var record = new Record({artist:'REM',title:"Out of Time", price: 9.99});
       store.add(record);
       assert.deepEqual(record, store.box[0]);
+      assert.equal(1, store.stock);
     });
 
-    it('should be able to remove a record', function() {
+    it('should be able to remove a record (and stock decreases)', function() {
       var store = new Store({name:'Rocking Ricks',city:"Inverness"});
       var record = new Record({artist:'REM',title:"Out of Time", price: 9.99});
       store.add(record);
       assert.equal(1, store.box.length);
+      assert.equal(1, store.stock);
       store.remove(record);
       assert.equal(0, store.box.length);
+      assert.equal(0, store.stock);
     });
 
 });
+
+
+describe('store cash', function() {
+  it('should start off with £100 cash', function() {
+    var store = new Store({name:'Rocking Ricks',city:"Inverness"});
+    assert.equal(100, store.cash);
+  });
+
+  it('should start off with £0 inventory', function() {
+    var store = new Store({name:'Rocking Ricks',city:"Inverness"});
+    assert.equal(100, store.cash);
+  });
+
+  it('should be able to add a record and inventory increases by cost of record', function() {
+    var store = new Store({name:'Rocking Ricks',city:"Inverness"});
+    var record = new Record({artist:'REM',title:"Out of Time", price: 9.99});
+    store.add(record);
+    assert.deepEqual(record, store.box[0]);
+    assert.equal(1, store.stock);
+    assert.equal(9.99, store.inventory);
+  });
+
+  it('should be able to remove a record and inventory decreases by cost of record', function() {
+    var store = new Store({name:'Rocking Ricks',city:"Inverness"});
+    var record = new Record({artist:'REM',title:"Out of Time", price: 9.99});
+    store.add(record);
+    assert.equal(9.99, store.inventory);
+    store.remove(record);
+    assert.equal(0, store.inventory);
+  });
+
+
+  });
+
--- store.js
+++ store.js
@@ -2,15 +2,22 @@ var Store = function(params) {
   this.name = params.name;
   this.city = params.city;
   this.box = [];
+  this.cash = 100;
+  this.stock = 0;
+  this.inventory = 0;
 };
 
 
 Store.prototype = {
   add: function(record) {
     this.box.push(record);
+    this.stock ++;
+    this.inventory += record.price;
   },
   remove: function(record) {
     this.box.pop(record);
+    this.stock --;
+    this.inventory -= record.price;
   }
 
 }
